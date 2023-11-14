f97839cc5f16e17c8833bb6982fdc54194791f9c
lakshay <lakshayyadavmain@gmail.com>
2023-11-14 21:49:42 +0530

    finalTesting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ethers } from "ethers";
 
 import OrganDonation from "./Contracts/OrganDonation.json";
@@ -34,12 +34,21 @@ function App() {
             method: "eth_requestAccounts",
           });
 
+          const currentURL = window.location.href;
+
+          const thirdSlashIndex = currentURL.indexOf('/', currentURL.indexOf('/') + 2);
+          let extractedPart;
+          if (thirdSlashIndex !== -1) {
+            extractedPart = currentURL.substring(0, thirdSlashIndex);
+
+          }
+
           window.ethereum.on("chainChanged", () => {
-            window.location.reload();
+            window.location.replace(extractedPart);
           });
 
           window.ethereum.on("accountsChanged", () => {
-            window.location.reload();
+            window.location.replace(extractedPart);
           });
 
           const provider = new ethers.providers.Web3Provider(ethereum);
@@ -65,24 +74,24 @@ function App() {
   return (
     <div>
       <Navbar bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home"><h1>Organ Donation</h1></Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <h6 className='alert alert-info'>Signed in with: {account}</h6>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Container>
+          <Navbar.Brand href="#home"><h1>Organ Donation</h1></Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <h6 className='alert alert-info'>Signed in with: {account}</h6>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Router>
         <Routes>
-          <Route path='/' element={<Landing/>}></Route>
+          <Route path='/' element={<Landing />}></Route>
           <Route path='/home' element={<Home _Contract={ContractState} Account={account} />} />
           <Route path='/donor-registration' element={<DonorRegistration DonationState={ContractState} />} />
           <Route path='/recipient-registration' element={<RecipientRegistration RecipientState={ContractState} />} />
-          <Route path='/donor-dashboard' element={<DonorDashboard _Contract={ContractState} Account={account}/>} />
-          <Route path='/recipient-dashboard' element={<RecipientDashboard _Contract={ContractState} Account={account}/>}></Route>
+          <Route path='/donor-dashboard' element={<DonorDashboard _Contract={ContractState} Account={account} />} />
+          <Route path='/recipient-dashboard' element={<RecipientDashboard _Contract={ContractState} Account={account} />}></Route>
         </Routes>
       </Router>
       <Footer></Footer>
